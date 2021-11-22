73c4843a285df73d5f88bb45a686be315993ede9
pentcle <pentacle@pentacle.xyz>
2021-11-22 22:02:44 +0000

    and it begins

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main>
+            <article>
+                <h2>People</h2>
+                <ul>
+                    <li>Yearn vaults</li>
+                </ul>
+            </article>
+            <article>
+                <h2>Partners</h2>
+                <ul>
+                    <li>Ecosystem partners</li>
+                    <li>Fee-sharing partners</li>
+                </ul>
+            </article>
+            <article>
+                <h2>Contributors</h2>
+                <ul>
+                    <li>Github open issues</li>
+                    <li>Bounties</li>
+                    <li>New contributor registration</li>
+                    <li>Pool of ideas</li>
+                    <li>Coordinape (for existing contributors)</li>
+                </ul>
+            </article>
+            <article>
+                <ul>
+                    <h2>Brand assets</h2>
+                    <li>Yearn mini guidelines and brand assets</li>s
+                </ul>
+            </article>
+        </main>
+    );
 }
 
 export default App;
